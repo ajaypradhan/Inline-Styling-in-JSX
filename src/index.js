fd94021e22c5bb36733be39299702a1c6937e538
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-08 14:35:19 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "red",
+  border: "10px solid red",
+  backgroundColor: "green"
+};
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
